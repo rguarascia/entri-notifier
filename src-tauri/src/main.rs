52c50b3e16fdd040754ebd4a59bec12ad9882118
Ryan Guarascia <rguarascia@gmail.com>
2022-06-05 13:24:51 -0400

    add close menu

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,8 +2,15 @@
   all(not(debug_assertions), target_os = "windows"),
   windows_subsystem = "windows"
 )]
+use tauri::{Menu, MenuItem, Submenu};
+
 fn main() {
+  let menu = Menu::new().add_submenu(Submenu::new(
+    "appname",
+    Menu::new().add_native_item(MenuItem::Quit),
+  ));
   tauri::Builder::default()
+    .menu(menu)
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
