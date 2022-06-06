3bf0b0b958eda9502bbde35763b8b1f1e2092520
Ryan Guarascia <rguarascia@gmail.com>
2022-06-06 11:04:58 -0400

    add theming, ui changes and logic for notifications when changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { notification } from "@tauri-apps/api";
 import {
   Button,
@@ -11,62 +11,151 @@ import {
   Tooltip,
 } from "@nextui-org/react";
 import { NextUIBadge } from "./components/NextUICustoms";
+import { iProvider } from "./types/iProviders";
+import useDarkMode from "use-dark-mode";
+import { darkTheme, lightTheme } from "./theme";
+import { MdDarkMode, MdOutlineLightMode } from "react-icons/md";
 
 function App() {
-  const [providers, setProviders] = useState<any>([]);
-  const displayNotification = () => {
-    const godaddy = providers.filter(
-      (provider: any) => provider.robot_code === "godaddy"
-    )[0];
+  const darkMode = useDarkMode();
 
-    notification.sendNotification({
-      title: "Status alert",
-      body: "Godaddy is online",
-      icon: godaddy?.icon,
+  const [providers, setProviders] = useState<iProvider[]>([]);
+  const [prevProviders, setPrevProviders] = useState<iProvider[]>([]);
+
+  const displayNotification = (provider: iProvider) => {
+    notification.isPermissionGranted().then((isGranted) => {
+      if (isGranted) {
+        notification.sendNotification({
+          title: "Status alert",
+          body: `${provider.name} is offline`,
+          icon: provider?.icon,
+        });
+      } else {
+        askForPermission();
+      }
     });
   };
 
-  useEffect(() => {
-    fetch("https://api.goentri.com/providers")
+  const askForPermission = () => {
+    notification.requestPermission();
+  };
+
+  const fetchProviders = async () => {
+    return await fetch("https://api.goentri.com/providers")
       .then((res) => res.json())
       .then((data) => {
-        setProviders(
-          data?.data?.map((provider: any) => {
-            return {
-              ...provider,
-              ui_status:
-                (provider.entri_automation_enabled && provider.dashboard_ui) ||
-                provider.domain_connect_enabled,
-            };
-          }) ?? []
+        return (
+          data?.data
+            ?.map((provider: any) => {
+              return {
+                ...provider,
+                ui_status:
+                  (provider.entri_automation_enabled &&
+                    provider.dashboard_ui) ||
+                  provider.domain_connect_enabled,
+              };
+            })
+            .sort((a: iProvider, b: iProvider) => {
+              return b.ui_status - a.ui_status;
+            }) ?? []
         );
+      })
+      .catch((err) => {
+        return err;
       });
+  };
+
+  useEffect(() => {
+    fetchProviders().then((data) => {
+      setProviders(data);
+    });
+
+    const interval = setInterval(() => {
+      fetchProviders().then((data) => {
+        if (JSON.stringify(data) !== JSON.stringify(prevProviders)) {
+          setProviders(data);
+        }
+      });
+    }, 60 * 1000 * 5); // every 5 minutes
+
+    return () => {
+      clearInterval(interval);
+    };
   }, []);
 
+  useEffect(() => {
+    if (!providers.length) return;
+    console.info("Checking for changes");
+    if (prevProviders.length === 0) {
+      setPrevProviders(providers);
+      return;
+    }
+
+    // Check if any provider has changed
+    const changedProviders = providers.filter(
+      (provider: iProvider) =>
+        prevProviders.filter(
+          (prevProvider: iProvider) =>
+            prevProvider.robot_code === provider.robot_code
+        )[0]?.ui_status !== provider.ui_status
+    );
+
+    changedProviders.forEach((element: iProvider) => {
+      displayNotification(element);
+    });
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [providers]);
+
   return (
-    <NextUIProvider>
+    <NextUIProvider theme={darkMode.value ? darkTheme : lightTheme}>
       <Container as={"main"} md fluid>
-        <Button
-          onClick={displayNotification}
+        <Grid
           css={{
-            m: "$10",
+            position: "absolute",
+            right: "$12",
+            top: "$12",
           }}
         >
-          Display notification
-        </Button>
+          <Tooltip
+            content={`Turn on ${darkMode.value ? "light" : "dark"} mode.`}
+            onClick={darkMode.toggle}
+            rounded
+            placement="leftEnd"
+            css={{
+              width: "fit-content",
+            }}
+          >
+            {darkMode.value ? (
+              <MdOutlineLightMode size={24} />
+            ) : (
+              <MdDarkMode size={24} />
+            )}
+          </Tooltip>
+        </Grid>
+
+        <Text h2 margin={`$10 0 $8 $8`}>
+          Entri Providers
+        </Text>
+
         <Grid.Container gap={2}>
           {providers?.map((provider: any) => (
             <Grid key={provider.robot_code} css={{ w: "200px" }}>
               <Card
                 css={{
-                  boxShadow: `0 1px 7px ${
+                  boxShadow: `0px 1px ${darkMode.value ? "14px" : "7px"} ${
                     provider.ui_status
                       ? "rgb(7 188 12 / 25%)"
                       : "rgb(241 196 15 / 25%)"
                   }`,
                 }}
               >
-                <Card.Body css={{ p: 0, backgroundColor: "$white" }}>
+                <Card.Body
+                  css={{
+                    p: 0,
+                    backgroundColor: darkMode.value ? "$accents8" : "$white",
+                  }}
+                >
                   <Card.Image
                     objectFit="contain"
                     src={provider.logo}
@@ -120,6 +209,17 @@ function App() {
             </Grid>
           ))}
         </Grid.Container>
+
+        <Button
+          size={"sm"}
+          light
+          onClick={askForPermission}
+          css={{
+            m: "$4",
+          }}
+        >
+          Reask for notification permission
+        </Button>
       </Container>
     </NextUIProvider>
   );
---
+++ src/theme/dark.tsx
@@ -0,0 +1,8 @@
+import { createTheme } from "@nextui-org/react";
+
+const darkTheme = createTheme({
+  type: "dark",
+  theme: {},
+});
+
+export default darkTheme;
